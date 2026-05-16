29648f7a9439b46c216e627f504d2524e13ef324
Abdullah Khan Asif <abdullahwarrich790@gmail.com>
2026-05-16 13:00:47 +0500

    Add files via upload

---
+++ Controller/Student.php
@@ -0,0 +1,8 @@
+<?php 
+include "../Models/Student.php";
+include "../Database.php";
+
+$db = new Database();
+$db_conn = $db->connect();
+$student_obj = new Student($db_conn);
+//$student-object=new Student(db->connect());
